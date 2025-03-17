f2f440e362343418391b8bee08178a8dbc1c94b9
Gil200 <pgilantunano@gmail.com>
2025-03-17 19:31:19 +0100

    Ej3visu.py

--- Ej3visu.py
+++ Ej3visu.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 st.title("Exercise Visualization: Airbnb Analysis")
-
+@st.cache_data
 def load_data():
     df = pd.read_csv("airbnb.csv")  # Ensure this file is in the same directory as the script
     df = df.rename(columns={"room_type": "listing_type", "neighbourhood": "neighborhood"})
